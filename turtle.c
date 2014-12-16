9adea1ffdff279d156715fe6298de6f87828afc9
Chau Duong <chau.duong1995@gmail.com>
2014-12-16 08:22:27 -0800

    Version 3 of turtle.c

--- turtle.c
+++ turtle.c
@@ -7,11 +7,11 @@ typedef struct
 	int age;
 } turtle;
 
-void happy_birthday(turtle *t)
+void happy_birthday(turtle *a)
 {
-	(*t).age = (*t).age + 1;
+	a->age = a->age + 1;
 	printf("Happy Birthday %s! You are now %i years old!\n",
-			(*t).name, (*t).age);
+			a->name, a->age);
 }
 
 int main ()
